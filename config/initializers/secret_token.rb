106354703fabb24d5b2a6082279e27274383740f
miio mitani <miio@miio-mitani-no-MacBook-Pro.local>
2012-06-28 01:24:24 +0900

    =初回コミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Signspot::Application.config.secret_token = 'b5999366c3c45a2e314a99577323ca395ce28ab00229c9a74858ebd2bc7dca791a1a61cffd230446d032211abb91cb96562fd39ab5c2a3d16694e799bfe5a0eb'
