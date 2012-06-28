f2ba928d0ad41eff0fccc93827d7840b1c85a9f5
miio mitani <miio@miio-mitani-no-MacBook-Pro.local>
2012-06-28 22:23:47 +0900

    肝心のスレタイが長くてもいいテストを付け忘れてたのでつけた

--- spec/models/signspot/thread_spec.rb
+++ spec/models/signspot/thread_spec.rb
@@ -6,4 +6,8 @@ describe Signspot::Thread do
     before { @thread = FactoryGirl.build(:start_thread) }
     it { @thread.save.should be_true }
   end
+  context "スレッドタイトルの長いスレッドを建てて文字が途切れないこと" do
+    before { @thread = FactoryGirl.create(:long_title_thread) }
+    it { @thread.title == FactoryGirl.attributes_for(:long_title_thread)[:title] }
+  end
 end
