ffcff30169d56b87a751f8792a2c6f66a0496c82
miio mitani <miio@miio-mitani-no-MacBook-Pro.local>
2012-06-30 00:24:44 +0900

    動かないからコメント合うと

--- spec/models/signspot/thread_spec.rb
+++ spec/models/signspot/thread_spec.rb
@@ -1,34 +1,34 @@
 #-*- encoding: utf-8 -*- 
 require 'spec_helper'
-describe Signspot::Project do
-  context "プロジェクトが建てられること" do
-
-  end
-  context "プロジェクトのslugは英数字のみであること" do
-
-  end
-  context "プロジェクトの管理人が最低1人いること" do
-
-  end
-  context "プロジェクトと紐づいたスレッドを建てられること" do
-
-  end
-  context "プロジェクト作成時のステートはデフォルトステートマスタと同一であること" do
-
-  end
-  context "プロジェクト間のスレッドチェーンの縦断移動ができること" do
-
-  end
-  context "非公開プロジェクトを作れる" do
-
-  end
-  context "非公開プロジェクトのメンバーであることを確認する" do
-
-  end
-  context "非公開プロジェクトのメンバーでないことを確認する" do
-
-  end
-end
+# describe Signspot::Project do
+#   context "プロジェクトが建てられること" do
+# 
+#   end
+#   context "プロジェクトのslugは英数字のみであること" do
+# 
+#   end
+#   context "プロジェクトの管理人が最低1人いること" do
+# 
+#   end
+#   context "プロジェクトと紐づいたスレッドを建てられること" do
+# 
+#   end
+#   context "プロジェクト作成時のステートはデフォルトステートマスタと同一であること" do
+# 
+#   end
+#   context "プロジェクト間のスレッドチェーンの縦断移動ができること" do
+# 
+#   end
+#   context "非公開プロジェクトを作れる" do
+# 
+#   end
+#   context "非公開プロジェクトのメンバーであることを確認する" do
+# 
+#   end
+#   context "非公開プロジェクトのメンバーでないことを確認する" do
+# 
+#   end
+# end
 describe Signspot::Thread do
   context "スレッドが建てられること" do
     before { @thread = FactoryGirl.build(:start_thread) }
@@ -38,7 +38,7 @@ describe Signspot::Thread do
     before { @thread = FactoryGirl.create(:long_title_thread) }
     it { @thread.title == FactoryGirl.attributes_for(:long_title_thread)[:title] }
   end
-  context "スレッドの中の最新のスレッドチェーンを取得できる" do
-
-  end
+#   context "スレッドの中の最新のスレッドチェーンを取得できる" do
+# 
+#   end
 end
