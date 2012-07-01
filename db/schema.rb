457ea7f24842e2a4a5401af1b7473dfded0d791d
miio mitani <miio@miio-mitani-no-MacBook-Pro.local>
2012-07-01 23:49:06 +0900

    テストを通すためのmigrationとか書いてみた

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120701135804) do
+ActiveRecord::Schema.define(:version => 20120701144620) do
 
   create_table "signspot_projects", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.text     "title"
+    t.string   "slug"
   end
 
   create_table "signspot_threads", :force => true do |t|
